11630fb915a476e7e8f20963c3257fbb850ae43c
Mukesh <ganatram@gmail.com>
2026-03-24 12:52:40 +0530

    updatedexpress

---
+++ serverInExpress/00-ml.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// custom 404 page
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 not found");
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 - Server Error");
+});
+
+app.listen(port, () => console.log(`express started on ${port} `));
