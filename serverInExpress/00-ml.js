144103c35d1c66dd13433a67558490ce8e66b793
Mukesh <ganatram@gmail.com>
2026-03-24 13:01:42 +0530

    updatedexpress

--- serverInExpress/00-ml.js
+++ serverInExpress/00-ml.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Route - home page
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send("Welcome to ML Travels");
+});
+
+//Route - about page
+app.get("/about", (req, res) => {
+  res.type("text/plain");
+  res.send("About - ML Travels");
+});
+
 // custom 404 page
 app.use((req, res) => {
+  // middleware - fallback
   res.type("text/plain");
   res.status(404);
   res.send("404 not found");
@@ -11,6 +24,7 @@ app.use((req, res) => {
 
 // custom 500 page
 app.use((err, req, res, next) => {
+  // middleware - fallback
   console.log(err.message);
   res.type("text/plain");
   res.status(500);
