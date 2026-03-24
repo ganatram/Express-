38aa8c9d9d46f3c4fb0d07b4154d1c7389af7804
Mukesh <ganatram@gmail.com>
2026-03-24 10:48:55 +0530

    updatedexpresscode

--- serverInNode/01-helloworld.js
+++ serverInNode/01-helloworld.js
@@ -5,6 +5,23 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
 
+  switch (path) {
+    case "":
+      res.writeHead(200, { "content-type": "text/plain" });
+      res.end("Home page");
+      break;
+
+    case "/about":
+      res.writeHead(200, { "content-type": "text/plain" });
+      res.end("About");
+      break;
+
+    default:
+      res.writeHead(404, { "content-type": "text/plain" });
+      res.end("Not Found");
+      break;
+  }
+
   /* res.writeHead(200, { "content-type": "text/plain" });
   res.end("hello world"); */
 });
